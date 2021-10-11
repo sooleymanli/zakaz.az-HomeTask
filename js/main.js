8ece07f844cce53799d4e27f1dd6dd120bc15d2f
Fuad Süleymanlı <suleymanli95@hotmail.com>
2021-10-11 09:04:09 +0400

    description finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,171 @@
+
+// Kredit kartı seçimi
+
+
+$(document).ready(function() {
+   
+       
+       $(".list-item").click((e)=>{
+           $("#cardChoose").html(e.target.innerHTML)
+           $("#cardChoose").css("color","#000")
+           $(".credits-part").slideDown(300)
+           }) 
+    
+        
+        $('.dropdown').click(function () {
+            $(this).attr('tabindex', 1).focus();
+            $(this).find('.dropdown-list').slideToggle(300);
+            
+        });
+        $('.dropdown').focusout(function () {
+          
+            $(this).find('.dropdown-list').slideUp(300);
+        });
+
+
+    });
+    
+    
+
+    // Kredit hesablama kalkulatoru
+
+    let creditRange = document.querySelector("#credit-range")
+let option = document.querySelectorAll(".option-item")
+document.querySelectorAll(".range-step").forEach(function (ctrl) {
+	var el = ctrl.querySelector('input');
+	var output = ctrl.querySelector('output');
+
+	el.oninput = function () {
+		
+        // rənglər
+		ctrl.querySelectorAll("option").forEach(function (opt) {
+			if (opt.value <= el.valueAsNumber) {
+				opt.style.backgroundColor = 'white';
+				opt.style.border = '1px solid #F58634'
+			}
+			else {
+				opt.style.backgroundColor = 'white';
+				opt.style.border = '1px solid #D0D0D0'
+			}
+		});
+	
+        //  əvvəl və sonrakı rənglər
+		var valPercent = (el.valueAsNumber - parseInt(el.min)) / (parseInt(el.max) - parseInt(el.min));
+		var style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(' +
+			valPercent + ', #F58634), color-stop(' +
+			valPercent + ', #F6F6F6));';
+		el.style = style;
+
+		option.forEach(e => {
+            // e.style.cssText="color:#2D2D2D;height:18px;width:18px;position:static;line-height:90px;font-size:14px;font-weight:400;border:0.5px solid #F58634 !important;"
+			e.style.color = "#2D2D2D";
+			e.style.height = "18px";
+			e.style.width = "18px"
+			e.style.position = "static"
+			e.style.lineHeight = "90px"
+			e.style.fontSize = "14px"
+			e.style.fontWeight = "400"
+			e.style.border = "0.5px solid #F58634 !important;"
+			e.innerHTML = e.value
+
+            if(e.value >= option[creditRange.value]){
+                e.style.border = "5.5px solid #F58634 !important;"
+
+            }
+
+
+		})
+
+		option[creditRange.value - 1].style.color = "#F58634"
+		option[creditRange.value - 1].style.width = "28px"
+		option[creditRange.value - 1].style.height = "28px"
+		option[creditRange.value - 1].style.position = "relative"
+		option[creditRange.value - 1].style.bottom = "5px"
+		option[creditRange.value - 1].style.lineHeight = "100px"
+		option[creditRange.value - 1].style.fontSize = "16px"
+		option[creditRange.value - 1].style.fontWeight = "bold"
+		option[creditRange.value - 1].style.border = "1px solid #F58634 !important;"
+
+		option[creditRange.value - 1].innerHTML = option[creditRange.value - 1].innerHTML + " " + "ay"
+
+		if ((' ' + ctrl.className + ' ').indexOf(' ' + 'range-step-popup' + ' ') > -1) {
+			var selectedOpt = ctrl.querySelector('option[value="' + el.value + '"]');
+			output.innerText = selectedOpt.text;
+			output.style.left = "50%";
+			selectedOpt.style.backgroundColor = "red"
+			selectedOpt.style.color = "red"
+			output.style.left = ((selectedOpt.offsetLeft + selectedOpt.offsetWidth / 2) - output.offsetWidth / 2) + 'px';
+		}
+	};
+	el.oninput();
+});
+
+window.onresize = function () {
+	document.querySelectorAll(".range").forEach(function (ctrl) {
+		var el = ctrl.querySelector('input');
+		el.oninput();
+	});
+};
+
+
+// Məhsul haqqənda məlumat
+
+let descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo..."
+
+$(document).ready(()=>{
+   
+    $("#text").html(
+        `${descText.substring(0,244)}...     <span class="read-more" >ardını oxu</span>`
+    )
+
+  
+
+    $(".read-more").click(()=>{
+        $("#text").html(
+            `${descText}`
+        )
+    })
+
+   
+    
+
+   // Aşağı enəndə  "Xüsusiyyətlər" və "rəylərin" görünməsi
+
+    $(document).scroll(()=>{
+        if(window.scrollY>=701){
+            $(".navbar").css("display","flex")
+        }else{
+            $(".navbar").css("display","none")
+
+        }
+
+
+    })
+  
+    //  "Xüsusiyyətlər" və "rəylərin" altına borderin çəkilməsi
+
+    $(document).scroll(()=>{
+        if(window.scrollY>=701 && window.scrollY<=1368){
+            $("#href-feature").css("border-bottom","3px solid #F58634")
+            $("#href-comments").css("border-bottom","none")
+        }else if(window.scrollY<=2000 && window.scrollY>=1368){
+            $("#href-comments").css("border-bottom","3px solid #F58634")
+            $("#href-feature").css("border-bottom","none")
+
+        }
+console.log(window.scrollY)
+
+    })
+  
+})
+
+
+
+
+
+
+
+
 // Sürətli sifariş bölməsi
 
 class PhoneField {
@@ -69,141 +237,3 @@ document.addEventListener("DOMContentLoaded", function () {
         b = [];
     for (let c = 0; c < a.length; c++) b.push(new PhoneField(a[c], a[c].dataset.phonemask, a[c].dataset.placeholder));
 });
-
-
-
-
-
-// Kredit kartı seçimi
-
-
-$(document).ready(function() {
-   
-       
-       $(".list-item").click((e)=>{
-           $("#cardChoose").html(e.target.innerHTML)
-           $("#cardChoose").css("color","#000")
-           $(".credits-part").slideDown(300)
-           }) 
-    
-        
-        $('.dropdown').click(function () {
-            $(this).attr('tabindex', 1).focus();
-            $(this).find('.dropdown-list').slideToggle(300);
-            
-        });
-        $('.dropdown').focusout(function () {
-          
-            $(this).find('.dropdown-list').slideUp(300);
-        });
-
-
-    });
-    
-    
-
-    // Aylıqa bölmə
-
-    let creditRange = document.querySelector("#credit-range")
-let option = document.querySelectorAll(".option-item")
-document.querySelectorAll(".range-step").forEach(function (ctrl) {
-	var el = ctrl.querySelector('input');
-	var output = ctrl.querySelector('output');
-
-	el.oninput = function () {
-		// rənglər
-		ctrl.querySelectorAll("option").forEach(function (opt) {
-			if (opt.value <= el.valueAsNumber) {
-				opt.style.backgroundColor = 'white';
-				opt.style.border = '1px solid #F58634'
-			}
-			else {
-				opt.style.backgroundColor = 'white';
-				opt.style.border = '1px solid #D0D0D0'
-			}
-		});
-		//  əvvəl və sonrakı rənglər
-		var valPercent = (el.valueAsNumber - parseInt(el.min)) / (parseInt(el.max) - parseInt(el.min));
-		var style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(' +
-			valPercent + ', #F58634), color-stop(' +
-			valPercent + ', #F6F6F6));';
-		el.style = style;
-
-
-
-		option.forEach(e => {
-			e.style.color = "#2D2D2D";
-			e.style.height = "18px";
-			e.style.width = "18px"
-			e.style.position = "static"
-			e.style.lineHeight = "90px"
-			e.style.fontSize = "14px"
-			e.style.fontWeight = "400"
-			e.style.border = "0.5px solid #F58634 !important;"
-			e.innerHTML = e.value
-
-            if(e.value >= option[creditRange.value]){
-                e.style.border = "5.5px solid #F58634 !important;"
-
-            }
-
-
-		})
-
-		option[creditRange.value - 1].style.color = "#F58634"
-		option[creditRange.value - 1].style.width = "28px"
-		option[creditRange.value - 1].style.height = "28px"
-		option[creditRange.value - 1].style.position = "relative"
-		option[creditRange.value - 1].style.bottom = "5px"
-		option[creditRange.value - 1].style.lineHeight = "100px"
-		option[creditRange.value - 1].style.fontSize = "16px"
-		option[creditRange.value - 1].style.fontWeight = "bold"
-		option[creditRange.value - 1].style.border = "1px solid #F58634 !important;"
-
-		option[creditRange.value - 1].innerHTML = option[creditRange.value - 1].innerHTML + " " + "ay"
-
-		if ((' ' + ctrl.className + ' ').indexOf(' ' + 'range-step-popup' + ' ') > -1) {
-			var selectedOpt = ctrl.querySelector('option[value="' + el.value + '"]');
-			output.innerText = selectedOpt.text;
-			output.style.left = "50%";
-			selectedOpt.style.backgroundColor = "red"
-			selectedOpt.style.color = "red"
-			output.style.left = ((selectedOpt.offsetLeft + selectedOpt.offsetWidth / 2) - output.offsetWidth / 2) + 'px';
-		}
-	};
-	el.oninput();
-});
-
-window.onresize = function () {
-	document.querySelectorAll(".range").forEach(function (ctrl) {
-		var el = ctrl.querySelector('input');
-		el.oninput();
-	});
-};
-
-
-let descText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo..."
-
-$(document).ready(()=>{
-   
-    $("#text").html(
-        `${descText.substring(0,244)}...     <span class="read-more" >ardını oxu</span>`
-    )
-
-  
-
-    $(".read-more").click(()=>{
-        $("#text").html(
-            `${descText}`
-        )
-    })
-
-   
-  
-})
-
-
-
-
-
-
