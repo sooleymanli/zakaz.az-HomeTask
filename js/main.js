c9579e2848ead2c3400b5b8d8eb150d02ef055d8
Fuad Süleymanlı <suleymanli95@hotmail.com>
2021-10-12 09:04:36 +0400

    updated mobile

--- js/main.js
+++ js/main.js
@@ -127,7 +127,65 @@ $(document).ready(()=>{
     })
 
    
-    
+   
+
+
+    // Rəng eçimi mobil üçün
+    $(".color-item").click((e)=>{
+        $(".color-name").html(e.target.innerHTML)
+      
+        $(".color-dropdown-list").slideUp(300)
+        }) 
+ 
+
+  $(".color-dropdown").click(()=>{
+      $(this).attr('tabindex', 1).focus();
+      $('.color-dropdown-list').slideToggle(300);
+
+
+  })
+  
+  $(".color-dropdown").focusout(()=>{
+     $('.color-dropdown-list').slideUp(300);
+});
+
+
+
+
+ // Yaddaş seçimi mobil üçün
+
+ $(".memory-item").click((e)=>{
+    $(".memory-value").html(e.target.innerHTML)
+  
+    $(".memory-dropdown-list").slideUp(300)
+    }) 
+
+
+$(".memory-dropdown").click(()=>{
+  $(this).attr('tabindex', 1).focus();
+  $('.memory-dropdown-list').slideToggle(300);
+
+
+})
+
+$(".memory-dropdown").focusout(()=>{
+ $('.memory-dropdown-list').slideUp(300);
+});
+
+
+
+
+
+// Mobil üçün xüsusiyyətlərin hamısının görülməsi
+
+$(".show-more-tr").click(()=>{
+    $(".show-more-tr").removeClass("shadow")
+    $(".mobile-product-feature-table .table table tr").css("display","table-row")
+    $(".mobile-product-feature-table-bottom").css("display","none")
+})
+
+
+
 
    // Aşağı enəndə  "Xüsusiyyətlər" və "rəylərin" görünməsi
 
@@ -136,6 +194,21 @@ $(document).ready(()=>{
             $(".navbar").css("display","flex")
         }else{
             $(".navbar").css("display","none")
+
+        }
+
+
+    })
+
+
+   // Mobil versiyada Aşağı enəndə  bottom-menunun  görünməsi
+
+    $(document).scroll(()=>{
+        if(window.scrollY>=860){
+            $(".mobile-fixed-bottom").fadeIn(400)
+        }else{
+           
+            $(".mobile-fixed-bottom").fadeOut(400)
 
         }
 
